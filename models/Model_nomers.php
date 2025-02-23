ea4bcbc1ef3677f44dab297f96ac1bcbe29a320d
Нелли <160469487+13563457@users.noreply.github.com>
2025-02-23 12:49:59 +0300

    Update Model_nomers.php

--- models/Model_nomers.php
+++ models/Model_nomers.php
@@ -1,1 +1,19 @@
-
+<?php
+class Model_nomers extends CI_Model
+{
+    //выбрать номер
+    public function select_nomers()
+    {
+        $sql = 'select * from nomers, type_nomer WHERE nomers.id_nomer = type_nomer.id_type';
+        $result = $this->db->query($sql);
+        return $result->result_array();
+    }
+    //добавить номер
+    public function ins_nomers( $namen, $discription,$price,$photo)
+    {
+        $sql = 'insert into nomers (namen, description,price,photo) ';
+        $result = $this->db->query($sql, array($namen, $discription,$price,$photo));
+        return $result;
+    }
+}
+?>
