58e6985aa82694b8adecc2e9cffcdd000b7b3799
Нелли <160469487+13563457@users.noreply.github.com>
2025-02-23 12:47:41 +0300

    Update Model_uslugi.php

--- models/Model_uslugi.php
+++ models/Model_uslugi.php
@@ -1,1 +1,19 @@
-
+<?php
+class Model_uslugi extends CI_Model
+{
+    //выбрать услугу
+    public function select_uslugi()
+    {
+        $sql = 'select * from uslugi';
+        $result = $this->db->query($sql);
+        return $result->result_array();
+    }
+    //добавить услугу
+    public function ins_uslugi($name_usluga, $opisanie,$price,$img)
+    {
+        $sql = 'insert into uslugi (name_usluga, opisanie,price,img) ';
+        $result = $this->db->query($sql, array($name_usluga,$opisanie, $price, $img));
+        return $result;
+    }
+}
+?>
