136a1226f03933b8397bd8f384d4415eb52e026a
Нелли <160469487+13563457@users.noreply.github.com>
2025-02-24 20:46:16 +0300

    Update Model_bron.php

--- models/Model_bron.php
+++ models/Model_bron.php
@@ -1,1 +1,12 @@
-
+<?php
+class Model_bron extends CI_Model
+{
+ //Отчёт сведения о номерах (передача результата контроллеру Buhgalter)
+    public function report_nomer($dates, $datef)
+    {
+        $sql = "SELECT id_bron,namen, price, status  FROM  bron, nomers WHERE bron.id_nomer = nomers.id_nomer and date_start BETWEEN ? and ? GROUP BY bron.id_bron , namen , price,status";
+        $result1 = $this->db->query($sql, array($dates, $datef));
+        return $result1->result_array();
+    }
+}
+?>
