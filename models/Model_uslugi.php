a5efebbe1c88956bc2d8bed38b184d4929dad509
Нелли <160469487+13563457@users.noreply.github.com>
2025-02-26 12:16:31 +0300

    Update Model_uslugi.php
     public function report_usluga($dates, $datef)

--- models/Model_uslugi.php
+++ models/Model_uslugi.php
@@ -15,5 +15,12 @@ class Model_uslugi extends CI_Model
         $result = $this->db->query($sql, array($name_usluga,$opisanie, $price, $img));
         return $result;
     }
+     //Отчёт о доходах от дополнительных услуг (передача результата контроллеру Buhgalter)
+    public function report_usluga($dates, $datef)
+    {
+        $sql = "SELECT name_usluga,ed_izm, price, count(id),sum(price)  FROM bron, bron_uslugi,uslugi WHERE bron_uslugi.id_uslugi = uslugi.id_uslugi and date_start BETWEEN ? and ? GROUP BY bron_uslugi.id_uslugi , name_usluga ,ed_izm, price";
+        $result1 = $this->db->query($sql, array($dates, $datef));
+        return $result1->result_array();
+    }
 }
 ?>
