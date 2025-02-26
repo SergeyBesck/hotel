f6a650f7e569dfbb27eb9eed00a8a745cabbcb31
Нелли <160469487+13563457@users.noreply.github.com>
2025-02-26 12:33:20 +0300

    Update navbar_dir_bukh.php
    Полностью заменила навигацию

--- views/temp/navbar_dir_bukh.php
+++ views/temp/navbar_dir_bukh.php
@@ -2,28 +2,22 @@
   <nav class="navbar">
     <div class="container">
       <div class="justify-content-sm-start">
-        <a class="navbar-brand" href="main"><img src="images/logo.jpg" alt="logo" width="185" height="60"></a>
+        <a class="navbar-brand" href="main"><img src="img/logo.jpg" alt="logo" width="185" height="60"></a>
       </div>
       <div class="justify-content-sm-center">
-        <p class="text-center">Оформить бронирование</p>
+        <p class="bukh"><b>ЛИЧНЫЙ КАБИНЕТ БУХГАЛТЕРА</b></p>
       </div>
       <div class="justify-content-sm-end">
         <div class="btn-group">
           <div class="dropdown">
-          <button class="btn btn-outline-dark dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Отчёты</button>
+            <button class="btn btn-outline-dark dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Отчёты</button>
             <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="">сводный отчёт </a></li>
-              <li><a class="dropdown-item" href="">сводный отчёт о доходах от доп. услуг </a></li>
+              <li><a class="dropdown-item" href="buhgalter/report_dohod">сводный отчёт </a></li>
+              <li><a class="dropdown-item" href="buhgalter/report_usluga">сводный отчёт о доходах от доп. услуг </a></li>
             </ul>
           </div>
         </div>
-        <?php
-          if($this->session->userdata('id_role') == 3){
-              echo '
-                <a href=""><button class="btn btn-outline-dark" type="submit">Сведения о комнатах </button></a>
-              ';
-          }
-        ?>
+        <a href="buhgalter/report_nomer"><button class="btn btn-outline-dark" type="submit">Сведения о комнатах </button></a>
         <a href="main/exit"><button class="btn btn-outline-dark" type="submit">Выйти</button></a>
       </div>
     </div>
