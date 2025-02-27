cb028310499a9e3d69ff0c8cc50cbd7b517ec8fc
Нелли <160469487+13563457@users.noreply.github.com>
2025-02-27 16:05:53 +0300

    Update Model_bron.php
    заменила функцию report_dohod()

--- models/Model_bron.php
+++ models/Model_bron.php
@@ -8,10 +8,10 @@ class Model_bron extends CI_Model
         $result1 = $this->db->query($sql, array($dates, $datef));
         return $result1->result_array();
     }
-  //Отчёт о доходах (передача результата контроллеру Buhgalter)
+ //Отчёт о доходах (передача результата контроллеру Buhgalter)
     public function report_dohod($dates, $datef)
     {
-        $sql = "SELECT id_bron,namen, price, count(id_bron),sum(price)  FROM  bron, nomers WHERE bron.id_nomer = nomers.id_nomer and date_start BETWEEN ? and ? GROUP BY bron.id_bron , namen , price";
+        $sql = "SELECT id_bron,namen, price,date_start,date_end, count(id_bron),sum(price)  FROM  bron, nomers WHERE bron.id_nomer = nomers.id_nomer and date_start BETWEEN ? and ? GROUP BY bron.id_bron , namen , price,date_start,date_end";
         $result1 = $this->db->query($sql, array($dates, $datef));
         return $result1->result_array();
     }   
