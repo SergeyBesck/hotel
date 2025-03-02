339cae4f0755437eb3c8f9f145f83f746fa6224a
Нелли <160469487+13563457@users.noreply.github.com>
2025-03-02 16:46:45 +0300

    Update Model_bron.php

--- models/Model_bron.php
+++ models/Model_bron.php
@@ -11,9 +11,10 @@ class Model_bron extends CI_Model
  //Отчёт о доходах (передача результата контроллеру Buhgalter)
     public function report_dohod($dates, $datef)
     {
-        $sql = "SELECT id_bron,namen, price,date_start,date_end, count(id_bron),sum(price)  FROM  bron, nomers WHERE bron.id_nomer = nomers.id_nomer and date_start BETWEEN ? and ? GROUP BY bron.id_bron , namen , price,date_start,date_end";
+        $sql = "SELECT id_bron,namen, price,date_start,date_end ,(date_end - date_start) AS date_difference ,sum(price) FROM  bron, nomers WHERE bron.id_nomer = nomers.id_nomer and date_start BETWEEN ? and ? GROUP BY bron.id_bron , namen , price,date_start,date_end";
         $result1 = $this->db->query($sql, array($dates, $datef));
         return $result1->result_array();
-    }   
+    }
+}
 }
 ?>
