6a0057385a3fcf8bb6a51e8c5f93c3f6459bc7dd
Нелли <160469487+13563457@users.noreply.github.com>
2025-03-03 16:19:12 +0300

    Update Model_bron.php
    Изменила сводный отчёт о доходах

--- models/Model_bron.php
+++ models/Model_bron.php
@@ -8,13 +8,12 @@ class Model_bron extends CI_Model
         $result1 = $this->db->query($sql, array($dates, $datef));
         return $result1->result_array();
     }
- //Отчёт о доходах (передача результата контроллеру Buhgalter)
+ //Сводный отчёт о доходах (передача результата контроллеру Buhgalter)
     public function report_dohod($dates, $datef)
     {
-        $sql = "SELECT id_bron,namen, price,date_start,date_end ,(date_end - date_start) AS date_difference ,sum(price) FROM  bron, nomers WHERE bron.id_nomer = nomers.id_nomer and date_start BETWEEN ? and ? GROUP BY bron.id_bron , namen , price,date_start,date_end";
+        $sql = "SELECT id_bron,namen, price,date_start,date_end ,(date_end - date_start) AS date_difference FROM  bron, nomers WHERE bron.id_nomer = nomers.id_nomer and date_start BETWEEN ? and ? GROUP BY bron.id_bron , namen , price,date_start,date_end";
         $result1 = $this->db->query($sql, array($dates, $datef));
         return $result1->result_array();
     }
-}
 }
 ?>
