bc29504e52c925f0aa950a2bb0b3b6780345c137
victoria <160472021+VictoriaShevchuk@users.noreply.github.com>
2025-03-11 13:08:06 +0300

    Модель для выбора номера при бронировании

--- models/Model_nomers.php
+++ models/Model_nomers.php
@@ -8,6 +8,26 @@ class Model_nomers extends CI_Model
         $result = $this->db->query($sql);
         return $result->result_array();
     }
+    //выбрать номера с данными на конкретную дату
+    public function select_nomers_info($data)
+    {
+        $sql = 'SELECT 
+            nomers.id_nomer, 
+            nomers.namen, 
+            nomers.price, 
+            CASE 
+                WHEN bron.id_bron IS NOT NULL AND bron.date_start <= ? AND bron.date_end >= ? THEN "занят" 
+                ELSE "свободен" 
+            END AS status
+        FROM 
+            nomers
+        JOIN 
+            type_nomer ON nomers.id_nomer = type_nomer.id_type
+        LEFT JOIN 
+            bron ON nomers.id_nomer = bron.id_nomer;';
+        $result = $this->db->query($sql, array($data,$data));
+        return $result->result_array();
+    }
     //добавить номер
     public function ins_nomers( $namen, $discription,$price,$photo)
     {
@@ -15,5 +35,21 @@ class Model_nomers extends CI_Model
         $result = $this->db->query($sql, array($namen, $discription,$price,$photo));
         return $result;
     }
+
+    public function select_nomers_booking($person, $id_type, $datas, $datae)
+    {
+        $sql = "SELECT DISTINCT nomers.id_nomer, nomers.namen, nomers.description, nomers.price, nomers.photo
+        FROM nomers
+        JOIN type_nomer ON nomers.type_nomer = type_nomer.id_type
+        LEFT JOIN bron ON bron.id_nomer = nomers.id_nomer
+        WHERE persona >= $person
+        AND id_type = $id_type
+        AND (bron.id_bron IS NULL 
+       OR bron.status = 'Отменена' OR (((bron.date_start < '$datas' AND bron.date_start > '$datae') OR (bron.date_end < '$datas'))))
+       ";
+        $result = $this->db->query($sql);
+        return $result->result_array();
+    }
 }
-?>
+?>   
+
