fca10af62456421410815781bbaded450a6871e0
victoria <160472021+VictoriaShevchuk@users.noreply.github.com>
2025-03-11 13:10:05 +0300

    Update Model_uslugi.php

--- models/Model_uslugi.php
+++ models/Model_uslugi.php
@@ -8,12 +8,16 @@ class Model_uslugi extends CI_Model
         $result = $this->db->query($sql);
         return $result->result_array();
     }
-    //добавить услугу
     public function ins_uslugi($name_usluga, $opisanie,$price,$img)
     {
         $sql = 'insert into uslugi (name_usluga, opisanie,price,img) ';
         $result = $this->db->query($sql, array($name_usluga,$opisanie, $price, $img));
         return $result;
+    }
+        public function select_dop_uslugi($id_user, $id_bron)
+    {
+        $sql = 'SELECT name_usluga, price, opisanie FROM bron LEFT JOIN bron_uslugi ON bron_uslugi.id_bron = bron.id_bron LEFT JOIN uslugi ON bron_uslugi.id_uslugi = uslugi.id_uslugi WHERE bron.id_user = ? AND bron.id_bron = ?';
+        return $this->db->query($sql, array($id_user, $id_bron))->result_array();
     }
 }
 ?>
